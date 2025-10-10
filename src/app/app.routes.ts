aa9265b367a7cec3530f7251432a2b3375d1541a
Hugopereezzz <hugopereezzzz@gmail.com>
2025-10-10 17:43:10 +0200

    liante

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { Contador } from './components/contador/contador';
 import { Matatopos } from './components/matatopos/matatopos';
 import { Carrera } from './components/carrera/carrera';
 import { PasoParametros } from './components/paso-parametros/paso-parametros';
-import { ListPersonajes } from './components/list-personajes/list-personajes';
 
 export const routes: Routes = [
     { path: 'contador', component: Contador},
     { path: 'matatopos', component: Matatopos},
     { path: 'carrera', component: Carrera},
     { path: 'pasoparametros', component: PasoParametros},
-    { path: 'ListPersonajes', component: ListPersonajes},
 
 ];
---
+++ src/model/Peronsaje.ts
@@ -0,0 +1,13 @@
+export class Personaje{
+    nombre: string;
+    raza: string;
+    poder: number;
+    imagen: string;
+    
+    constructor(nombre:string, raza:string, poder:number, imagen:string ){
+        this.imagen=imagen;
+        this.nombre=nombre;
+        this.poder=poder;
+        this.raza=raza;
+    }
+}
